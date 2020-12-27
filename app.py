88901960f49462383cd3c62fbc79b7b22ae540fa
nanditha16 <nanditha.murthy@gmail.com>
2020-12-27 17:06:12 -0500

    Python basic- pycharm start

---
+++ app.py
@@ -0,0 +1,21 @@
+print("Nanditha") # String
+print("o----")
+print(" ||||")
+price=10 # integers
+print(price)
+price=20.4 # float
+print(price)
+is_newPatient = True #boolean - key words
+print(is_newPatient)
+
+# scan from user:
+name = input('What is your name? ')
+color = input('What is your fav colour? ')
+print("Hi " + name + ". Your fav colour is : "+ color)
+
+# str to int
+birth_year = input('What is your birth year? ')
+age = 2020 - int(birth_year)
+print(type(age))
+print(age)  # can only concatenate str (not "int") to str
+

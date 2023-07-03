090d64a1cdb161c8824efe2d0023d1c852cc7c7e
nanditha16 <nanditha.murthy@gmail.com>
2023-07-03 01:54:03 -0400

    Python revisit from start

--- calculator.py
+++ calculator.py
@@ -1,29 +1,56 @@
 """
-- datatype :int
+- datatype :int, float
 - Arithmetic  (+-*/%)
 - interactive mode
 """
-numFlavor = input("what type are the numbers? ")
+numFlavor = input("what type are the numbers? [int, float] ")
+
+if numFlavor not in ('int', 'float'):
+    print("Invalid number type. Please select 'int' or 'float'.")
+    exit(1)
 
 match numFlavor:
     case 'int':
-        x = int(input("What's x? "))
-        y = int(input("What's y? "))
+        try:
+            x = int(input("What's x? "))
+            y = int(input("What's y? "))
+        except ValueError:
+            print("Invalid input. Please enter valid integers.")
+            exit(1)
     case 'float':
-        x = float(input("What's x? "))
-        y = float(input("What's y? "))
+        try:
+            x = float(input("What's x? "))
+            y = float(input("What's y? "))
+        except ValueError:
+            print("Invalid input. Please enter valid floats.")
+            exit(1)
 
-operation = input("What operation? ")
+operation = input("What operation? [+-*/%]")
 def switch(operation):
     if operation == "+":
-        return x+y
+        return x + y
     elif operation == "-":
-        return x-y
+        return x - y
     elif operation == "*":
-        return x*y
+        return x * y
     elif operation == "/":
-        return x/y
+        try:
+            if y == 0:
+                raise ZeroDivisionError
+            return x / y
+        except ZeroDivisionError:
+            print("Error: Division by zero is not allowed.")
+            exit(1)
     elif operation == "%":
-        return x%y
+        try:
+            if y == 0:
+                raise ZeroDivisionError
+            return x % y
+        except ZeroDivisionError:
+            print("Error: Division by zero is not allowed.")
+            exit(1)
+    else:
+        print("Unsupported operation. Please select one of '+', '-', '*', '/', '%'.")
+        exit(1)
 
-print("result", switch(operation))
+print("Result:", switch(operation))

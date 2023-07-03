5472b91f37452c2cd41e9d4c6fc72a5bacf7a614
nanditha16 <nanditha.murthy@gmail.com>
2023-07-03 01:41:51 -0400

    Python revisit from start

--- calculator.py
+++ calculator.py
@@ -8,10 +8,10 @@ numFlavor = input("what type are the numbers? ")
 match numFlavor:
     case 'int':
         x = int(input("What's x? "))
-        y = int(input("What's 7? "))
+        y = int(input("What's y? "))
     case 'float':
         x = float(input("What's x? "))
-        y = float(input("What's 7? "))
+        y = float(input("What's y? "))
 
 operation = input("What operation? ")
 def switch(operation):

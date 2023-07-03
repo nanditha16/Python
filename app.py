0bab80fa7e75317ca555c2401fba7c821b8734b8
nanditha16 <nanditha.murthy@gmail.com>
2023-07-03 01:24:07 -0400

    Python basic revisit

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import datetime
+
+# CS50 Intro to Python s
 print("Nanditha") # String
 print("o----")
 print(" ||||")
@@ -9,13 +12,39 @@ is_newPatient = True #boolean - key words
 print(is_newPatient)
 
 # scan from user:
-name = input('What is your name? ')
-color = input('What is your fav colour? ')
-print("Hi " + name + ". Your fav colour is : "+ color)
+# remove whitespace from str and capitalize just 1st letter
+name = input('What is your name? ').strip().title()
+first,last = name.split(" ")
 
-# str to int
+color = input('What is your fav colour? ').strip().capitalize()
+print("Hi " + first + ". Your fav colour is :", color)
+
+#
+"""
+- str to int
+- datetime library 
+- print function handling str vs int, multiple usage
+"""
+today = datetime.date.today()
+print(today)
+year = today.year
+
 birth_year = input('What is your birth year? ')
-age = 2020 - int(birth_year)
+print(type(birth_year))
+
+age = year - int(birth_year)
 print(type(age))
-print(age)  # can only concatenate str (not "int") to str
+# Will not work: print("You are "+ age)  # can only concatenate str (not "int") to str
+# named parameters:  end , sep sent to print function
+print("You are ", end="") # override print new line to continue
+print(age)
+print(f"hello, {first}")
+print("\"age\"", age, sep=":")
+print('"Just a number!"')
+
+"""
+- datatype :int 
+- Arithmetic  (+-*/%)
+- interactive mode 
+""" 
 

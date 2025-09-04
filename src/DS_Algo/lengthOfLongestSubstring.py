8dbbf723d8b83f69a5b2fb3997a36b82c55bf31d
nanditha16 <nanditha16@users.noreply.github.com>
2025-09-04 18:51:58 +0000

    length if longest Substring

--- src/DS_Algo/lengthOfLongestSubstring.py
+++ src/DS_Algo/lengthOfLongestSubstring.py
@@ -3,6 +3,9 @@
 # 0 <= s.length <= 5 * 104
 # s consists of English letters, digits, symbols and spaces.
 
+#  O(n) time where n is the length of the string
+#  O(min(n, m)) space, where m is the size of the character set.
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         char_index = {}  # keeps track of the last seen index of each character.
@@ -10,9 +13,12 @@ class Solution:
         max_length = 0 
 
         for right in range(len(s)):
+            # If a character is repeated and its last seen index is within the current 
+            # window, move left to one position after that index.
             if s[right] in char_index and char_index[s[right]] >= left:
                 left = char_index[s[right]] + 1
             char_index[s[right]] = right
+            # Update max_length with the size of the current window.
             max_length = max(max_length, right - left + 1)
 
         return max_length

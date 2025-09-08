0cd1511871ab9688489776fdaf5fa7c0afee1b85
nanditha16 <nanditha16@users.noreply.github.com>
2025-09-08 16:45:25 +0000

    Given an array of integers nums, find the next lexicographically permutation of nums.

--- src/DS_Algo/nextLexicographicalPermutation.py
+++ src/DS_Algo/nextLexicographicalPermutation.py
@@ -9,7 +9,7 @@
 # For example, the next permutation of arr = [1,2,3] is [1,3,2].
 # Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
 # While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
-# Given an array of integers nums, find the next permutation of nums.
+# Given an array of integers nums, find the next lexicographically permutation of nums.
 
 # The replacement must be in place and use only constant extra memory.
 
